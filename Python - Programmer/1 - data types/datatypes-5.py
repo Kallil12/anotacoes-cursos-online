0b36bb55034d5969a7ed0730a96db32e328dce15
Kallil12 <kallilaraujo@gmail.com>
2020-05-13 22:37:44 -0300

    começando o curso de analista de dados com python

---
+++ Python - Programmer/1 - data types/datatypes-5.py
@@ -0,0 +1,72 @@
+# exercise 1
+# Import the datetime object from datetime
+from datetime import datetime
+
+# Iterate over the dates_list 
+for date_str in dates_list:
+    # Convert each date to a datetime object: date_dt
+    date_dt = datetime.strptime(date_str,'%m/%d/%Y')
+    
+    # Print each date_dt
+    print(date_dt)
+# ----//---- ----//---- ----//---- ----//---- ----//---- ----//---- ----//---- ----//---- ----//----
+# exercise 2
+# Loop over the first 10 items of the datetimes_list
+for item in datetimes_list[:10]:
+    # Print out the record as a string in the format of 'MM/DD/YYYY'
+    print(item.strftime('%m/%d/%Y'))
+    
+    # Print out the record as an ISO standard string
+    print(item.isoformat())
+# ----//---- ----//---- ----//---- ----//---- ----//---- ----//---- ----//---- ----//---- ----//----
+# exercise 3
+# Create a defaultdict of an integer: monthly_total_rides
+monthly_total_rides = defaultdict(int)
+
+# Loop over the list daily_summaries
+for daily_summary in daily_summaries:
+    # Convert the service_date to a datetime object
+    service_datetime = datetime.strptime(daily_summary[0], '%m/%d/%Y')
+
+    # Add the total rides to the current amount for the month
+    monthly_total_rides[service_datetime.month] += int(daily_summary[4])
+    
+# Print monthly_total_rides
+print(monthly_total_rides)
+# ----//---- ----//---- ----//---- ----//---- ----//---- ----//---- ----//---- ----//---- ----//----
+# exercise 4
+# Import datetime from the datetime module
+from datetime import datetime
+
+# Compute the local datetime: local_dt
+local_dt = datetime.now()
+
+# Print the local datetime
+print(local_dt)
+
+# Compute the UTC datetime: utc_dt
+utc_dt = datetime.utcnow()
+
+# Print the UTC datetime
+print(utc_dt)
+# ----//---- ----//---- ----//---- ----//---- ----//---- ----//---- ----//---- ----//---- ----//----
+# exercise 5
+# Create a Timezone object for Chicago
+chicago_usa_tz = timezone('US/Central')
+
+# Create a Timezone object for New York
+ny_usa_tz = timezone('US/Eastern')
+
+# Iterate over the daily_summaries list
+for orig_dt, ridership in daily_summaries:
+
+    # Make the orig_dt timezone "aware" for Chicago
+    chicago_dt = orig_dt.replace(tzinfo=chicago_usa_tz)
+    
+    # Convert chicago_dt to the New York Timezone
+    ny_dt = chicago_dt.astimezone(ny_usa_tz)
+    
+    # Print the chicago_dt, ny_dt, and ridership
+    print('Chicago: %s, NY: %s, Ridership: %s' % (chicago_dt, ny_dt, ridership))
+# ----//---- ----//---- ----//---- ----//---- ----//---- ----//---- ----//---- ----//---- ----//----
+# exercise 6
